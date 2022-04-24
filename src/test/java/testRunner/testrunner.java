685389b97a243873d7ce3e058c3e2c8918e6fb65
mohammedarifs <arifshaik321@gmail.com>
2022-04-25 00:46:26 +0530

    adding project to github

---
+++ src/test/java/testRunner/testrunner.java
@@ -0,0 +1,15 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="./src/test/resources/Feature", glue={"stepdefination"},
+plugin= {"pretty","html:target/reports/cucmber.html","pretty","junit:target/reports/cucumber.html"}, monochrome = true)
+
+public class testrunner {
+		
+	
+}
