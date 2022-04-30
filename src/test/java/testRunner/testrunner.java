a138ebd31aa5a0fcc9c37d70f75ad8fe72fc89d1
mohammedarifs <arifshaik321@gmail.com>
2022-04-30 12:36:47 +0530

    Adding changes for webhook 4

--- src/test/java/testRunner/testrunner.java
+++ src/test/java/testRunner/testrunner.java
@@ -9,6 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="./src/test/resources/Feature", glue={"stepdefination"},
 plugin= {"pretty","html:target/reports/cucmber.html","junit:target/reports/cucumber.html","json:target/cucumber.json"}, monochrome = true)
 //testing code step 3
+//Testing for 4
 public class testrunner {
 		
 	
